ec6b9a7d627b0c7df1b2070b7023f82dd67e42ff
asliddin <asliddin>
2022-03-09 19:34:20 +0300

    petshop

---
+++ script/script.js
@@ -0,0 +1,111 @@
+// window.scrollTo(100, 10000);
+
+window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY
+    if (scrollY == 100 || scrollY == 150 || scrollY == 200 || scrollY == 250 || scrollY == 300 || scrollY == 350 || scrollY == 400) {
+        window.scrollTo(100, 640)
+    } else if (scrollY == 940) {
+        window.scrollTo(940, 1430)
+    }
+})
+
+let burger = document.querySelector('.navbar-button');
+let burgerMenu = document.querySelector('.navbar__list');
+let menu = document.querySelector('.menu');
+let menu_img = document.querySelector('.navbar-button img');
+let menu_x = document.querySelector('.menu_x');
+
+burger.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active')
+})
+menu_x.addEventListener('click', () => {
+    burgerMenu.classList.remove('active')
+})
+
+let cards = document.querySelector('.cards'),
+    card = document.querySelectorAll('.card'),
+    cardss = document.querySelector('.cardss');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > cards.offsetTop - cards.clientHeight) {
+        for (let i = 0; i < card.length; i++) {
+            const el = card[i];
+            el.style = `animation: a 1.5s linear 0s 1;   opacity: 1;`
+        }
+    }
+})
+
+let a = document.querySelector(':root'),
+    color = document.querySelector('.color'),
+    header = document.querySelector('header'),
+    navbar = document.querySelector('.navbar__list'),
+    logoBtn = document.querySelector('.logo_img'),
+    logoBtn2 = document.querySelector('.logo_img2'),
+    intro_center = document.querySelector('.intro_center'),
+    body = document.querySelector('.yellow'),
+    comp = getComputedStyle(body, "-webkit-scrollbar-thumb")
+    
+color.addEventListener('click', () => {
+    color.classList.toggle('green')
+    if (color.classList.contains('green')) {
+        header.style = `background:green;`
+        navbar.style = `background:green;`
+        a.style.setProperty('--yellow', 'green')
+        logoBtn.setAttribute('src', './images/logo_green.png')
+        logoBtn2.setAttribute('src', './images/logo2_green.png')
+        intro_center.style = `background: rgba(12, 121, 55, 0.5);`
+        if (body.classList.contains('yellow')) {
+            body.classList.remove('yellow')
+            body.classList.add('green')
+        }
+
+    } else {
+        header.style = `background:var(--yellow);`
+        navbar.style = `background:var(--yellow);`
+        a.style.setProperty('--yellow', '#F4CB38')
+        logoBtn.setAttribute('src', './images/logo.png')
+        logoBtn2.setAttribute('src', './images/logo2.png')
+        intro_center.style = `background: rgba(166, 121, 55, 0.5);`
+        body.style = `background: linear-gradient(124.13deg, #F4CB38 10.97%, #F4A938 77.98%)`
+        if (body.classList.contains('green')) {
+            body.classList.remove('green')
+            body.classList.add('yellow')
+        }
+    }
+})
+
+const heart_icons = document.querySelectorAll('.h')
+
+for (let i = 0; i < heart_icons.length; i++) {
+    const heart_icon = heart_icons[i];
+    heart_icon.addEventListener('click', function on() {
+        heart_icon.classList.add('fa-solid')
+        heart_icon.classList.remove('fal')
+    })
+    heart_icon.addEventListener('dblclick', () => {
+        heart_icon.classList.add('fal')
+        heart_icon.classList.remove('fa-solid')
+    })
+}
+const never = document.querySelector('.never')
+never.addEventListener('click', () => {
+    location.reload()
+})
+
+let modalWindow = document.querySelector('.intro_center');
+let modalClose = modalWindow.querySelector('.x');
+let modal = modalWindow.querySelector('.subs');
+logoBtn.addEventListener('click', () => {
+    modalWindow.style.display = 'flex';
+    setTimeout(() => {
+        modalWindow.classList.add('active')
+        modal.classList.add('active')
+    }, 500);
+})
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('active')
+    setTimeout(() => {
+        modalWindow.style.display = 'none'
+    }, 200);
+})
